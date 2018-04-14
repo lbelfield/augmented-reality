61b6019aee6acc888445d6ab322d6d290067c896
Luke Belfield <luke.belfield@thetrainline.com>
2018-04-14 22:00:28 +0100

    Test file

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,22 @@
-import React, {Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import ReactDOM from 'react-dom';
-import { render } from 'react-dom'
+import { AFrameRenderer, Marker } from 'react-web-ar';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="container-fluid">
-              <h1>About</h1>
-              <p>This application uses React, Redux, React Routrer and a variety of other helpful libraries</p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <AFrameRenderer arToolKit={{ sourceType: 'webcam', sourceUrl: './images/hiro_marker.png'}} >
+        <Marker parameters={{ preset: 'hiro' }}>
+          <a-box color="blue" material="opacity: 1;" position="0 0.09 0" scale="0.4 0.8 0.8">
+            <a-animation attribute="rotation" to="360 0 0" dur="5000" easing="linear" repeat="indefinite" />
+          </a-box>
+        </Marker>
+      </AFrameRenderer>
+    );
+  }
 }
 
-render(<App />, document.getElementById('root'))
+render(<App />, document.getElementById('root'));
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 import 'babel-polyfill';
-import React, { Component } from 'react'
-import { render } from 'react-dom';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './styles/styles.css'; // Webpack can import CSS files too!
+import createHistory from 'history/createBrowserHistory';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-import { AFrameRenderer, Marker } from 'react-web-ar'
+import './styles/styles.css'; // Webpack can import CSS files too!
+import App from './components/App';
 
-class ReactArApp extends Component {
-  render() {
-    return (
-      <AFrameRenderer arToolKit={{ sourceType: 'webcam', sourceUrl: './images/hiro_marker.png'}} >
-        <Marker parameters={{ preset: 'hiro' }}>
-          <a-box color='blue' material='opacity: 1;' position="0 0.09 0" scale='0.4 0.8 0.8'>
-            <a-animation attribute="rotation" to="360 0 0" dur="5000" easing="linear" repeat="indefinite" />
-          </a-box>
-        </Marker>
-      </AFrameRenderer>
-    )
-  }
-}
+const history = createHistory();
 
-render(<ReactArApp />, document.getElementById('root'))
+const app = (
+  <App history={history}/>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
