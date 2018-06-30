6ff2c72bdb156d8edfa41671dfa9070f8134344a
Luke Belfield <luke.belfield@thetrainline.com>
2018-06-30 14:25:39 +0100

    Railcard demo - do not merge to master

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,11 @@ import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 
 // ifconfig -> en0 -> inet -> https/x:3000
-
-// import Hiro from './hiro/Hiro';
-// import Torus from './torus/Torus';
-// import RollingScopes from './rollingScopes/RollingScopes';
-// import Earth from './earth/Earth';
-// import TrainTicket from './trainTicket/TrainTicket';
 import Railcard from './railcard/Railcard';
 
 class App extends Component {
   render() {
     return (
-      // <Hiro />
-      // <Torus />
-      // <Earth />
-      // <RollingScopes />
-      // <TrainTicket />
       <Railcard />
     );
   }
