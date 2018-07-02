5ff46461803c0acbe93bf790536af2db0a4843c6
Luke Belfield <luke.belfield@thetrainline.com>
2018-07-02 15:38:26 +0100

    live d

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 
 // ifconfig -> en0 -> inet -> https/x:3000
+
 import Railcard from './railcard/Railcard';
 
 class App extends Component {
