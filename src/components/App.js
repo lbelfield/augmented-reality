d286e9863d3609c4311ae1786d9674bcdc41f83b
Luke Belfield <luke.belfield@thetrainline.com>
2018-07-02 15:35:35 +0100

    live demo 3

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,12 @@ import ReactDOM, { render } from 'react-dom';
 
 // ifconfig -> en0 -> inet -> https/x:3000
 
-import Hiro from './hiro/Hiro';
-// import Earth from './earth/Earth';
-// import TrainTicket from './trainTicket/TrainTicket';
+import TrainTicket from './trainTicket/TrainTicket';
 
 class App extends Component {
   render() {
     return (
-      <Hiro />
-      // <Earth />
-      // <TrainTicket />
+      <TrainTicket />
     );
   }
 }
